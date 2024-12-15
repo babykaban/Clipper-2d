8d5d00b10abccc81a058db143047c29d1e56db53
BabyKaban <paul92219@proton.me>
2024-12-14 20:02:19 -0800

    Adding and testing the original clipper2 library, the hint for the bug is when the result is a few polygons and not one, should step through it few times because it is not obvious what is that

--- code/build.bat
+++ code/build.bat
@@ -1,11 +1,21 @@
 @echo off
 
+set BuildPath=..\
+
+set CommonCompilerFlags2=-Zi -W4 -EHsc -MT -nologo -GR- -Gm- -EHa- -Od -Oi -WX -wd4456 -wd4201 -wd4100 -wd4189 -FC  
+set CommonLinkerFlags2= -opt:ref user32.lib gdi32.lib winmm.lib
+
 set CommonCompilerFlags= -Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -wd4456 -wd4146 -FC -Z7
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib
 
-IF NOT EXIST ..\build mkdir ..\build
-pushd ..\build
+IF NOT EXIST %BuildPath%\build mkdir %BuildPath%\build
+pushd %BuildPath%\build
 
 REM 64-bit build
 cl %CommonCompilerFlags% ..\code\main.cpp -Fmmain.map /link %CommonLinkerFlags%
 cl %CommonCompilerFlags% ..\code\generate_polygon_file.cpp -Fmmain.map /link %CommonLinkerFlags%
+
+cl %CommonCompilerFlags2% /std:c++17  ..\code\test_clipper2.cpp /link %CommonLinkerFlags2% 
+
+popd
+
---
+++ code/test_clipper2.cpp
@@ -0,0 +1,165 @@
+/* ========================================================================
+   $File: $
+   $Date: 2024 $
+   $Revision: $
+   $Creator: BabyKaban $
+   $Notice:  $
+   ======================================================================== */
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "main.h"
+
+#define PROFILER 1
+#define PRINT_OUT_RESULT 1
+
+#define USINGZ 0
+#include "clipper2.h"
+
+#include "profiler.cpp"
+#include "clipper.engine.cpp"
+//#include "clipper.offset.cpp"
+//#include "clipper.rectclip.cpp"
+
+struct v2_f32
+{
+    f32 x;
+    f32 y;
+};
+
+struct polygon
+{
+    u32 Count;
+    v2_f32 *Points;
+};
+
+struct polygon_set
+{
+    u32 PolyCount;
+    polygon *Polygons;
+};
+
+inline void
+IntersectTwoPolies(polygon *S, polygon *C)
+{
+    TimeFunction;
+    using namespace Clipper2Lib;
+
+    Assert((S->Count > 0) && (C->Count > 0));
+
+    PathsD subject, clip, solution;
+
+    PathD tmp0;
+    for(u32 I = 0; I < S->Count; ++I)
+    {
+        tmp0.push_back({S->Points[I].x, S->Points[I].y});        
+    }
+    subject.push_back(tmp0);
+
+    
+    PathD tmp1;
+    for(u32 I = 0; I < C->Count; ++I)
+    {
+        tmp1.push_back({C->Points[I].x, C->Points[I].y});        
+    }
+    clip.push_back(tmp1);
+
+    solution = Intersect(subject, clip, FillRule::EvenOdd);
+    
+#if PRINT_OUT_RESULT
+    printf("\nTest: %s, %s", ClipTypes[1], FillRules[0]);
+
+    for (const PathD& path : solution) {
+        for (const PointD& pt : path) {
+            std::cout << "(" << pt.x << ", " << pt.y << "),";
+        }
+        std::cout << std::endl;
+    }
+
+    printf("\n");
+#endif
+}
+
+internal void
+ReadPolies(polygon_set *Subjects, polygon_set *Clips, char *FileName)
+{
+    TimeFunction;
+
+    FILE *In;
+    fopen_s(&In, FileName, "rb");
+    if(In)
+    {
+        u32 SubjectsIdentifier = 0;
+        fread(&SubjectsIdentifier, sizeof(u32), 1, In);
+        Assert(SubjectsIdentifier == 0xFFFF0000);
+
+        fread(&Subjects->PolyCount, sizeof(u32), 1, In);
+        Assert(Subjects->PolyCount != 0);
+
+        Subjects->Polygons = (polygon *)malloc(sizeof(polygon)*Subjects->PolyCount);
+        for(u32 I = 0; I < Subjects->PolyCount; ++ I)
+        {
+            polygon *Poly = Subjects->Polygons + I;
+            fread(&Poly->Count, sizeof(u32), 1, In);
+            Assert(Poly->Count != 0);
+
+            Poly->Points = (v2_f32 *)malloc(sizeof(v2_f32)*Poly->Count);
+            
+            fread(Poly->Points, sizeof(v2_f32)*Poly->Count, 1, In);
+#if PRINT_READ
+            printf("Read Subject %d completed, v_count: %d\n", I, Poly->Count);
+#endif
+        }
+
+        u32 ClipsIdentifier = 0;
+        fread(&ClipsIdentifier, sizeof(u32), 1, In);
+        Assert(ClipsIdentifier == 0x0000FFFF);
+
+        fread(&Clips->PolyCount, sizeof(u32), 1, In);
+        Assert(Clips->PolyCount != 0);
+
+        Clips->Polygons = (polygon *)malloc(sizeof(polygon)*Clips->PolyCount);
+        for(u32 I = 0; I < Clips->PolyCount; ++ I)
+        {
+            polygon *Poly = Clips->Polygons + I;
+            fread(&Poly->Count, sizeof(u32), 1, In);
+            Assert(Poly->Count != 0);
+
+            Poly->Points = (v2_f32 *)malloc(sizeof(v2_f32)*Poly->Count);
+            
+            fread(Poly->Points, sizeof(v2_f32)*Poly->Count, 1, In);
+#if PRINT_READ
+            printf("Read Clip %d completed, v_count: %d\n", I, Poly->Count);
+#endif
+        }
+    }    
+
+    fclose(In);
+}
+
+int main()
+{
+    BeginProfile();
+
+    polygon_set Subjects = {};
+    polygon_set Clips = {};
+
+    //"c:/Paul/Clipper-2d/output/polygons_b.bin"
+    ReadPolies(&Subjects, &Clips, "c:/Paul/Clipper-2d/output/polygons_b.bin");
+
+    for(u32 I = 0; I < 1; ++I)
+    {
+//        polygon *S = Subjects.Polygons + I;
+//        polygon *C = Clips.Polygons + I;
+
+        polygon *S = Subjects.Polygons + 4;
+        polygon *C = Clips.Polygons + 4;
+
+        IntersectTwoPolies(S, C);
+    }
+
+    EndAndPrintProfile();
+    
+    return 0;
+}
